d52908673b65d559cbc7f847a0313ee6562129bc
xiaogua07 <xiaogua07@outlook.com>
2024-05-19 13:42:20 +0800

    尝试设置返回值类型

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,21 @@
  */
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// Make the fetch request to the third party API endpoint
-  const response = await fetch('https://devapi.qweather.com/v7/weather/3d?location=120.15,30.31&key=' + env.qweatherkey, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    // Make the fetch request to the third party API endpoint
+    const response = await fetch('https://devapi.qweather.com/v7/weather/3d?location=120.15,30.31&key=' + env.qweatherkey, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-  // Retrieve the data from the response
-  const data = await response.json();
+    // Retrieve the data from the response
+    const data = await response.json();
 
-  // Use the data to modify or manipulate your content as needed
-  return new Response(data as any);
-	},
+    // Use the data to modify or manipulate your content as needed
+    const res = new Response(data as any);
+    res.headers.set('Content-Type', 'application/json');
+    return res;
+  },
 };
